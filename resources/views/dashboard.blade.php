4594d24778ca4dd0e12a76588a2bac34ab4d359e
Dannn08 <danielzhang585@gmail.com>
2024-11-27 15:18:52 +0700

    amazing_footer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,9 @@
             {{ __('Category') }}
         </h2> --}}
     </x-slot>
-
+<head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
     <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -24,9 +26,11 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @include('category.card')
-                    @include('footer')
                 </div>
             </div>
         </div>
+    </div>
+    <div class="py-0">
+        @include('footer')
     </div>
 </x-app-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -393,97 +393,8 @@
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-    <footer class="bg-[#45526e] dark:bg-gray-900" >
-    <div class="p-4 py-6 lg:py-8">
-          <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" class="h-10 me-3 my-4" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-              </a>
-          </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4">
-              <div>
-                  <h2 class="mb-6 text-xs font-semibold text-white uppercase dark:text-white">Lorem ipsum dolor sit amet</h2>
-              </div>
-              <div>
-                <h2 class="mb-6 text-8 font-semibold text-white uppercase">Product</h2>
-                  <ul class="text-gray-400 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Free</a>
-                      </li>
-                      <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Premium</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-white uppercase">Features</h2>
-                  <ul class="text-gray-400 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline">Video</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">Artikel</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                <h2 class="mb-6 text-sm font-semibold text-white uppercase">Contact Us</h2>
-                <ul class="text-gray-400 dark:text-gray-400 font-medium">
-                    <li class="mb-4 flex items-center">
-                        <svg class="w-5 h-5 mr-2 text-gray-400 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M22 4H2C1.448 4 1 4.448 1 5v14c0 .552.448 1 1 1h20c.552 0 1-.448 1-1V5c0-.552-.448-1-1-1zm-1 2l-9 6-9-6h18z"/>
-                          </svg>
-                          <a href="mailto:contact@daanc-corps.com" class="hover:underline">contact@daanc-corps.com</a>                          
-                        <li class="flex items-center">
-                            <svg class="w-5 h-5 mr-2 text-gray-400 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M22 16.92c-1.21 0-2.32-.35-3.2-1.03l-1.43 1.43c-.8.8-2.09.8-2.88 0l-3.62-3.62c-.8-.8-.8-2.09 0-2.88l1.43-1.43c-.67-.88-1.03-1.99-1.03-3.2 0-2.42 1.33-4.5 3.28-5.79l-.98-1.68c-1.7-.32-3.58.18-4.69 1.3L8.1 4.14C7.12 5.26 7.6 7.14 8.92 8.46L11.4 10.94c-.8.8-2.09.8-2.88 0l-3.62-3.62c-.8-.8-.8-2.09 0-2.88l1.43-1.43C3.35 2.32 2.72 1 2.72 0a7.9 7.9 0 0 0 7.02 5.18c3.23 3.23 4.59 4.79 8.42 8.61-1.49 2.2-2.79 4.4-4.57 6.19-1.59 1.59-3.44 2.72-5.35 3.35C2.98 19.53 3 20 3 20s.39-2.27 2.08-3.71c2.24-2.25 5.06-5.12 5.18-5.2"/>
-                              </svg>
-                              <a href="tel:+000-000-000" class="hover:underline">+000-000-000</a>                                                                                                                                                                                                                                                                                          
-                        </li>
-                </ul>
-            </div>
-          </div>
-      </div>
-      <div class="text-left ml-3">
-        <h1 class="font-bold text-lg text-white mb-4">Alamat</h1>
-      </div>
-      <div class="relative w-full h-[400px] overflow-hidden"> 
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" class="w-full h-full object-cover" alt="Image">
-      </div>
-      <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-            <div class="flex mt-4 sm:justify-center sm:mt-0">
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                          <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                      </svg>
-                    <span class="sr-only">Facebook page</span>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
-                          <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
-                      </svg>
-                    <span class="sr-only">Discord community</span>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
-                      <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
-                  </svg>
-                    <span class="sr-only">Twitter page</span>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                    <svg class="w-8 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="8 0 20 20">
-                        <path fill-rule="evenodd" d="M12 2.163c3.42 0 3.84.013 5.188.075 1.38.065 2.56.385 3.495 1.32 1.23 1.231 1.255 2.813 1.32 3.495.062 1.348.075 1.768.075 5.188s-.013 3.84-.075 5.188c-.065 1.38-.385 2.56-1.32 3.495-1.231 1.231-2.813 1.255-3.495 1.32-1.348.062-1.768.075-5.188.075s-3.84-.013-5.188-.075c-1.38-.065-2.56-.385-3.495-1.32-1.23-1.231-1.255-2.813-1.32-3.495-.062-1.348-.075-1.768-.075-5.188s.013-3.84.075-5.188c.065-1.38.385-2.56 1.32-3.495 1.231-1.231 2.813-1.255 3.495-1.32C8.16 2.176 8.58 2.163 12 2.163zm0-2.163c-3.42 0-3.84.014-5.188.075-1.546.072-2.88.406-3.99 1.516-1.104 1.104-1.44 2.444-1.516 3.99-.062 1.348-.075 1.768-.075 5.188s.013 3.84.075 5.188c.075 1.546.412 2.886 1.516 3.99 1.104 1.11 2.444 1.444 3.99 1.516 1.348.062 1.768.075 5.188.075s3.84-.013 5.188-.075c1.546-.072 2.886-.406 3.99-1.516 1.104-1.104 1.44-2.444 1.516-3.99.062-1.348.075-1.768.075-5.188s-.013-3.84-.075-5.188c-.072-1.546-.412-2.886-1.516-3.99-1.104-1.11-2.444-1.444-3.99-1.516C15.84.014 15.42 0 12 0zm0 5.838a6.16 6.16 0 1 0 6.16 6.16A6.167 6.167 0 0 0 12 5.838zm0 10.528a4.368 4.368 0 1 1 4.368-4.368 4.373 4.373 0 0 1-4.368 4.368zm6.496-9.254a1.04 1.04 0 1 0-1.04-1.04 1.04 1.04 0 0 0 1.04 1.04z" clip-rule="evenodd"/>
-                      </svg>                      
-                    <span class="sr-only">Instagram</span>
-                </a>
-                </a>
-          </div>
-      </div>
-    </div>
-</footer>
 </body>
-
+<div class="py-0">
+    @include('footer')
+</div>
 </html>
